bb797474aeece0bf13a2ec192148e5b3dd114c80
Saahil Pandya <saahilpandya824@gmail.com>
2025-05-05 22:35:59 +0530

    add server logic and htmx template

--- a_vero/views.py
+++ a_vero/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from a_vero.models import *
+from .forms import *
 
-# Create your views here.
+
+@login_required
 def chat_view(request):
-    return render(request, 'a_vero/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name = "public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatMessageCreateForm()
+
+    if request.htmx:
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit = False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message' : message,
+                'user' : request.user
+            }
+            return redirect(request, 'a_vero/partials/chat_message_p.html', context)
+    return render(request, 'a_vero/chat.html', {'chat_messages' : chat_messages, 'form': form})
