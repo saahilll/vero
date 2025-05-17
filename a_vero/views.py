23acb0eb949291df359add25c65f355544b5b692
Saahil Pandya <saahilpandya824@gmail.com>
2025-05-18 00:21:17 +0530

    add file upload support

--- a_vero/views.py
+++ a_vero/views.py
@@ -1,7 +1,9 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from a_vero.models import *
 from .forms import *
 
@@ -132,3 +134,23 @@ def chatroom_delete_view(request, chatroom_name):
         return redirect('home')
 
     return render(request,'a_vero/chatroom_delete.html', {'chat_group': chat_group})
+
+def chat_file_upload(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+
+    if request.htmx and request.FILES:
+        file = request.FILES['file']
+        message = GroupMessage.objects.create(
+            file = file,
+            author = request.user,
+            group = chat_group,
+        )
+        channel_layer = get_channel_layer()
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(channel_layer.group_send)(
+            chatroom_name, event
+        )
+    return HttpResponse()
